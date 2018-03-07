5eb12f4a89019c4ab7b784a37754bedd439e9b67
Paul Craig <paul_craig_16@hotmail.com>
2018-03-07 16:43:59 -0500

    Make a change to see that the deployment works

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,15 +5,15 @@ import renderer from 'react-test-renderer'
 import App from '../pages/index.js'
 
 describe('With Enzyme', () => {
-  it('App shows "Hello world!"', () => {
+  it('App shows "Fully-automated luxury deployments!"', () => {
     const app = shallow(<App />)
 
-    expect(app.find('p').text()).toEqual('Hello World!')
+    expect(app.find('p').text()).toEqual('Fully-automated luxury deployments!')
   })
 })
 
 describe('With Snapshot Testing', () => {
-  it('App shows "Hello world!"', () => {
+  it('App shows "Fully-automated luxury deployments!"', () => {
     const component = renderer.create(<App />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
