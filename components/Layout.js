0d3ad5a7864f8c53849a51eeee575483b1f5f649
Paul Craig <paul_craig_16@hotmail.com>
2018-03-13 20:41:57 -0400

    Add title to Layout
    We are hardnosed, hyper-rational programmers.
    We don't need <title>With Emotion</title>.

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,28 @@
+import Head from 'next/head'
 import React from 'react'
 import AlphaBanner from '../components/AlphaBanner'
 import FederalBanner from '../components/FederalBanner'
 import Footer from '../components/Footer'
 import Link from 'next/link'
 
-const layoutStyle = {
+const layoutStyle = {}
 
-}
+const Layout = props => (
+  <div style={layoutStyle}>
+    <Head>
+      <title>{props.title || 'EnerGuide API'}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
 
-const Layout = (props) => (
+    <header>
+      <AlphaBanner />
+      <FederalBanner />
+    </header>
 
-    <div style={layoutStyle}>
-      <header>
-				<AlphaBanner />
-				<FederalBanner />
-			</header>
-
-		  <main role="main">
-      {props.children}
-			</main>
-			<Footer />
-    </div>
+    <main role="main">{props.children}</main>
+    <Footer />
+  </div>
 )
 
 export default Layout
